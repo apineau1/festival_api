1dfc99e98bc3d46f5acd50fd8e3cb4cc0a167524
apineau1 <58475031+apineau1@users.noreply.github.com>
2020-05-03 21:16:19 +0200

    Delete representations.php

--- representations.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('Dao.php');
-$lesRepresentations = Dao::getAllRepresentations();
-$array = array();
-$array["representations"]=$lesRepresentations;
-echo(json_encode($array));
-?>
